bf1454cb03f3db15fb966f053811388bb76573b7
chadrackmbupiokajess <chadrackmbupiokajess@gmail.com>
2026-05-14 17:48:56 +0100

    ajout de serie

--- app/tv/[id]/season/[seasonNumber]/episode/[episodeNumber]/page.tsx
+++ app/tv/[id]/season/[seasonNumber]/episode/[episodeNumber]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getTvDetails, getTvSeasonDetails, getTvEpisodeDetails } from "../../../../../../lib/tmdb";
+import { getTvDetails, getTvSeasonDetails, getTvEpisodeDetails } from "../../../../../../../../../../lib/tmdb";
 import Image from "next/image";
 import Link from "next/link";
 
